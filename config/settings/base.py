f17b549ce4582cb53e133648b3b6e40d2d6adf75
felamax101-code <felamax101@gmail.com>
2026-03-07 17:29:38 +0300

    host

--- config/settings/base.py
+++ config/settings/base.py
@@ -668,6 +668,7 @@ CORS_ALLOWED_ORIGINS=[
     "http://localhost:8000",
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "smartcomm.pythonanywhere.com",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
